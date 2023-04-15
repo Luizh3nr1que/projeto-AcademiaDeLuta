365af5562afdfc80d40f8dd7a9425f320b839ff4
luizh3nrique <luizh3nrique00@gmail.com>
2023-04-15 18:08:10 -0300

    Fix - mudança no css

--- javaS.js
+++ javaS.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function () {
     var header = document.querySelector(".header");
-    if (window.scrollY > 100) {
+    if (window.scrollY > 200) {
         header.classList.add("scroll");
     } else {
         header.classList.remove("scroll");
